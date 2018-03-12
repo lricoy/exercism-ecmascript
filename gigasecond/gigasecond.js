4be7bd6edfea96b4e45413cd4f1fd69047688e93
lricoy <ricoy.lucas@gmail.com>
2018-03-12 23:39:20 +0100

    second iteration on gigasecond

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -2,12 +2,11 @@
  *
  * @param {Date} date
  */
-export default function Gigasecond(date) {
-    let myGigaSecond = new Date(date);
-    myGigaSecond.setUTCSeconds(date.getSeconds() + 1000000000);
+export default date => {
+    const myGigaSecond = new Date(date.getTime() + 10 ** 12);
     return {
         date() {
             return myGigaSecond;
         },
     };
-}
+};
