ae9067afe1df950347e8b94c8085ec4b28fe0b8d
Lucas Ricoy <ricoy.lucas@gmail.com>
2018-03-31 22:47:48 +0200

    Pascal triangle recursive solution

--- pascals-triangle/pascals-triangle.js
+++ pascals-triangle/pascals-triangle.js
@@ -1,21 +1,26 @@
 export default size => {
-  let rows = [];
-  if (size === 1) {
-    rows = [[1]];
-  } else if (size === 2) {
-    rows = [[1], [1, 1]];
-  } else {
-    rows = [[1], [1, 1]];
-    for (let i = 1; i < size; i++) {
-      rows[i] = [1];
-      for (let j = 1; j <= i - 1; j++) {
-        rows[i].push(rows[i - 1][j - 1] + rows[i - 1][j]);
-      }
-      rows[i].push(1);
-    }
-  }
+  let rows = triangle(size);
+
   return {
     rows,
     lastRow: rows[rows.length - 1]
   };
 };
+
+function triangle(n) {
+  if (n === 0) {
+    return [];
+  }
+  if (n === 1) {
+    return [[1]];
+  }
+  const previousTriangle = triangle(n - 1);
+  const lastRow = previousTriangle[previousTriangle.length - 1];
+  const row = Array.from(Array(n), (elem, i) => {
+    if (i === 0 || i === n - 1) {
+      return 1;
+    }
+    return lastRow[i - 1] + lastRow[i];
+  });
+  return [...previousTriangle, row];
+}
